09ef038a84336ca03bd1b30268ede08ad4dcd6b3
Amit Kumar Mondal <admin@amitinside.com>
2016-01-19 17:42:07 +0100

    Added Paths Directors Stream to Observable

--- com.amitinside.rxjava.practice/src/main/java/com/amitinside/rxjava/practice/CreatingObservablesUsingFrom.java
+++ com.amitinside.rxjava.practice/src/main/java/com/amitinside/rxjava/practice/CreatingObservablesUsingFrom.java
@@ -15,8 +15,13 @@
  *******************************************************************************/
 package com.amitinside.rxjava.practice;
 
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
+import com.google.common.base.Throwables;
 import com.google.common.collect.Lists;
 
 import rx.Observable;
@@ -27,6 +32,19 @@ public final class CreatingObservablesUsingFrom {
 		final List<String> strings = Lists.newArrayList("a", "b", "c", "d");
 		final Observable<String> observable = Observable.from(strings);
 		observable.subscribe(System.out::println);
+		observable.subscribe(alphabet -> System.out.println(alphabet + "+"), System.out::println, System.out::println);
+		pathPbservable();
+	}
+
+	private static void pathPbservable() {
+		final Path resources = Paths.get("src", "main", "java");
+
+		try (final DirectoryStream<Path> dStream = Files.newDirectoryStream(resources);) {
+			final Observable<Path> dirObservable = Observable.from(dStream);
+			dirObservable.subscribe(System.out::println);
+		} catch (final Exception e) {
+			Throwables.propagate(e);
+		}
 	}
 
 }
